a8d22302a108c540592c5a12e69df4286e7e850a
Austine Mwangi <austinemwangideveloper@gmail.com>
2023-12-28 17:15:51 +0300

    added splash screen

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,7 @@ import { Text, View } from '../components/Themed';
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+      <Text style={styles.title}> Uhh ohh,,Nothing for you today</Text>
       <View
         style={styles.separator}
         lightColor='#eee'
